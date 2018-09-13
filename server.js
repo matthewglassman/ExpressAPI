eac2ea6736a357629d8699bb572e5f734a8f5066
matthewglassman <matthewglassman78@gmail.com>
2018-09-13 14:52:55 -0400

    Create Express App, Middleware, and first route

--- server.js
+++ server.js
@@ -2,3 +2,19 @@ const express = require('express')
 const logger = require('morgan')
 const errorhandler = require('errorhandler')
 const bodyParser = require('body-parser')
+
+let store = {}
+store.accounts = []
+
+//Create express app, middleware for parsing bod and error logging
+let app = express()
+
+app.use(bodyParser.json())
+app.user(logger('dev'))
+app.use(errorhandler())
+
+//GET route to /accounts which will send entire accounts array back to client
+
+app.get('/accounts', (req, res) => {
+  res.status(200).send(store.accounts)
+})
